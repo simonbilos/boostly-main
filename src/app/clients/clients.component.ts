97b90f448be241afc28b5a10b23b826b1e9a901a
Šimon Biloš <bilos.simon@gmail.com>
2025-08-07 11:57:53 +0200

    when screen is smaller than 900px, only one card is visible, added script to change cards

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
   selector: "app-clients",
@@ -6,4 +6,27 @@ import { Component } from "@angular/core";
   templateUrl: "./clients.component.html",
   styleUrl: "./clients.component.css",
 })
-export class ClientsComponent {}
+export class ClientsComponent {
+  private _cards = signal([
+    { id: 1, content: "karta 1" },
+    { id: 2, content: "karta 2" },
+    { id: 3, content: "karta 3" },
+  ]);
+
+  private _currentIndex = signal<number>(0);
+
+  currentIndex = computed(() => this._currentIndex());
+  cards = computed(() => this._cards());
+
+  prevCard() {
+    this._currentIndex.update((i) =>
+      i <= 0 ? this._cards().length - 1 : i - 1
+    );
+  }
+
+  nextCard() {
+    this._currentIndex.update((i) =>
+      i >= this._cards().length - 1 ? 0 : i + 1
+    );
+  }
+}
