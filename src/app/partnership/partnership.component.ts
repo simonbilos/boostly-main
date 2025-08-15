d43916bf1008967161d607cad961c95ca1605cd9
Šimon Biloš <bilos.simon@gmail.com>
2025-08-15 16:49:31 +0200

    new component

---
+++ src/app/partnership/partnership.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: "app-partnership",
+  imports: [],
+  templateUrl: "./partnership.component.html",
+  styleUrl: "./partnership.component.css",
+})
+export class PartnershipComponent {}
