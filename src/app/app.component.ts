3e12b3ca469468accc1955e7414c1778f0c2ede8
Šimon Biloš <bilos.simon@gmail.com>
2025-08-15 15:04:09 +0200

    added contact branch manually, because there are conflicts (screwed up 2nd time :( )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { ClientCardComponent } from "./client-card/client-card.component";
+import { ClientsComponent } from "./clients/clients.component";
 
 @Component({
   standalone: true,
@@ -8,6 +10,7 @@ import { RouterOutlet } from "@angular/router";
 
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
+  imports: [NavMenuComponent, ClientsComponent],
 })
 export class AppComponent {
   title = "boostly-main";
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from "@angular/forms";
+
+import { inject } from "@angular/core";
 
 @Component({
-  selector: 'app-contact',
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  selector: "app-contact",
+  imports: [ReactiveFormsModule],
+  templateUrl: "./contact.component.html",
+  styleUrl: "./contact.component.css",
 })
 export class ContactComponent {
+  private formBuilder = inject(FormBuilder);
 
+  contactForm = this.formBuilder.group({
+    userName: this.formBuilder.group({
+      firstName: [""],
+      lastName: [""],
+    }),
+    userEmail: ["", [Validators.required, Validators.email]],
+    userPhone: [""],
+    messageContent: [""],
+  });
+
+  // contactForm = new FormGroup({
+  //   userEmail: new FormControl(""),
+  //   userText: new FormControl(""),
+  // });
+
+  onSubmit() {
+    console.warn(this.contactForm.value);
+  }
+
+  // updateValue() {
+  //   this.contactForm.patchValue({
+  //     userEmail: "kirbis@gmail.com",
+  //     userText: {
+  //       textObject: "pomoc s dizajnom",
+  //       textContent: "ahoj, potrebujem helfnut",
+  //     },
+  //   });
+  // }
+
+  errorMessage(data: string) {
+    console.log(`${data} is not valid`);
+  }
 }
